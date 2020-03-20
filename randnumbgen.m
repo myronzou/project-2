668218910b96e51a586ae11d89acb0ae68ec1c8f
myronzou <myron_zou@yahoo.com>
2020-03-19 23:30:18 -0700

    Update randnumbgen.m

--- randnumbgen.m
+++ randnumbgen.m
@@ -5,13 +5,14 @@
 %size of weighted matrix
 
 INPUT = train0;
-%a = -2.4./length(test0);
+
 a = -0.5;
-%b = 2.4./length(test0);
+
 b = 0.5;
-r = (b-a).*rand(length(INPUT).^2,2) + a;
-WEIGHT = r;
+r = (b-a).*rand(2,2) + a;
+%numbers within rand are the dims of r
+%r will be a random matrix or array within bounds
 
 r_range = [min(r) max(r)]
-%This tests the random number generator by seeing if the min and max values
+% ^ This tests the random number generator by seeing if the min and max values
 %are within bounds.
